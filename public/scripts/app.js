80186a2367e35e7a54903b91f57bef1ad1a29698
askata992 <askata9213@gmail.com>
2018-11-19 23:35:58 +0200

    added socket.io and toastr

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,55 @@
 'use strict'
 
-let $switches = $('.input-switch');
-
-$switches.each(function (index) {
-    let $self = $(this);
-        getStatus(index, function (res, status) {
-            if (res.relayStatus === 'ON') {
-                $self[0].checked = true;
-            }
-        })
+$(document).ready(function () {
+    var socket = io();
+    // A dialog box is displayed when the server sends us a "message"
+    socket.on('message', function (message) {
+        toastr["info"](message);
+    })
 });
 
-$switches.change(function() {
+const $switches = $('.input-switch');
+const host = window.location.host;
+
+$switches.each(function (index) {
+    const $self = $(this);
+    getStatus(index, function (res, status) {
+        if (res.relayStatus === 'ON') {
+            $self[0].checked = true;
+        }
+    })
+});
+
+$switches.change(function () {
     console.log(this.id);
     let relayNumber;
     switch (this.id) {
         case 'switch1':
             relayNumber = 0
             break;
-            case 'switch2':
+        case 'switch2':
             relayNumber = 1
             break;
-            case 'switch3':
+        case 'switch3':
             relayNumber = 2
             break;
-            case 'switch4':
+        case 'switch4':
             relayNumber = 3
             break;
-    
+
         default:
             break;
     }
-    toggleStatus(relayNumber, function (res, status) {
-        if(status === 'success') {
-            let timeout = setTimeout(function() {
-            clearTimeout(timeout);
-            $('.alert').removeClass('show');
-            }, 3000);
-            // alert(res);
-            
-            $('.alert').addClass('show');
-            timeout();
+    toggleStatus(relayNumber);
 
-        } else {
-            alert('Error');
-        }
-    });
-    
 });
 
-function toggleStatus(relayNumber,status1) {
+function toggleStatus(relayNumber, status1) {
     let callback = function (res, status) {
         if (res.relayStatus === 'ON') {
-            $.post("http://192.168.0.100:3000/off/" + [relayNumber], status1);
+            $.post("http://" + host + "/off/" + [relayNumber], status1);
         } else {
-             $.post("http://192.168.0.100:3000/on/" + [relayNumber], status1);
+            $.post("http://" + host + "/on/" + [relayNumber], status1);
         }
     }
 
@@ -62,5 +57,5 @@ function toggleStatus(relayNumber,status1) {
 }
 
 function getStatus(relayNumber, callback) {
-    $.get("http://192.168.0.100:3000/status/" + [relayNumber],callback)
+    $.get("http://" + host + "/status/" + [relayNumber], callback)
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-
 const express = require('express');
 const app = express();
+const http = require('http');
 const port = 3000;
 const Gpio = require('onoff').Gpio;
 const relays = [new Gpio(17, 'high'), new Gpio(18, 'high'),new Gpio(27, 'high'),new Gpio(22, 'high')];
+const server = app.listen(port);
+const io = require('socket.io').listen(server);
 
 app.get('/status/:relayNumber', function (req, res) {
   let relayNumber = parseInt(req.params.relayNumber); 
@@ -21,6 +23,7 @@ app.post('/on/:relayNumber', function (req, res) {
   let relayNumber = parseInt(req.params.relayNumber);  
   console.log('Turn ON relay #' + relayNumber);
   relays[relayNumber].writeSync(0);
+  io.emit('message', 'Relay #' + (relayNumber+1) + ' was turned ON');
   res.send('Relay #' + (relayNumber+1) + ' was turned ON');;
 });
 
@@ -28,9 +31,27 @@ app.post('/off/:relayNumber', function (req, res) {
   let relayNumber = parseInt(req.params.relayNumber);
   console.log('Turn OFF relay #' + relayNumber);
   relays[relayNumber].writeSync(1);
+  io.emit('message', 'Relay #' + (relayNumber+1) + ' was turned OFF');
   res.send('Relay #' + (relayNumber+1) + ' was turned OFF');
 });
 
 app.use(express.static('public'));
+app.use('/libs', express.static('node_modules'));
 
-app.listen(port, () => console.log(`Rellay app listening on port ${port}!`));
+
+
+// Sockets
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', 'You are connected!');
+    socket.broadcast.emit('message', 'Another client has just connected!');
+
+    socket.on('message', function (message) {
+        console.log('A client is speaking to me! They\'re saying: ' + message);
+    }); 
+});
