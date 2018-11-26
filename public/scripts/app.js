2419dab05ec0071abb60fb073c2cbabe06c67fa0
askata992 <askata9213@gmail.com>
2018-11-26 22:38:11 +0200

    safe commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,38 @@
 'use strict'
 
 $(document).ready(function () {
-    var socket = io();
+    const $switches = $('.input-switch');
+    const host = window.location.host;
+    const socket = io();
+
+    $switches.click(function (e) {
+        e.preventDefault;
+        toggleStatus(this.dataset.relaynumber);
+    });
+
+    function toggleStatus(relayNumber, status1) {
+        let callback = function (res, status) {
+            if (res.relayStatus === 'ON') {
+                $.post("http://" + host + "/off/" + [relayNumber], status1);
+            } else {
+                $.post("http://" + host + "/on/" + [relayNumber], status1);
+            }
+        }
+
+        getStatus(relayNumber, callback);
+    }
+
+    function getStatus(relayNumber, callback) {
+        $.get("http://" + host + "/status/" + [relayNumber], callback)
+    }
+
     // A dialog box is displayed when the server sends us a "message"
     socket.on('message', function (message) {
         toastr["info"](message);
-    })
-});
-
-const $switches = $('.input-switch');
-const host = window.location.host;
-
-$switches.each(function (index) {
-    const $self = $(this);
-    getStatus(index, function (res, status) {
-        if (res.relayStatus === 'ON') {
-            $self[0].checked = true;
-        }
-    })
-});
-
-$switches.change(function () {
-    console.log(this.id);
-    let relayNumber;
-    switch (this.id) {
-        case 'switch1':
-            relayNumber = 0
-            break;
-        case 'switch2':
-            relayNumber = 1
-            break;
-        case 'switch3':
-            relayNumber = 2
-            break;
-        case 'switch4':
-            relayNumber = 3
-            break;
-
-        default:
-            break;
-    }
-    toggleStatus(relayNumber);
+    });
+    // Change switch status when server send us a "statuschange"
+    socket.on('statuschange', function (data) {
+        $switches[data.relayNumber].checked = !data.relayStatus;        
+    });
 
 });
-
-function toggleStatus(relayNumber, status1) {
-    let callback = function (res, status) {
-        if (res.relayStatus === 'ON') {
-            $.post("http://" + host + "/off/" + [relayNumber], status1);
-        } else {
-            $.post("http://" + host + "/on/" + [relayNumber], status1);
-        }
-    }
-
-    getStatus(relayNumber, callback);
-}
-
-function getStatus(relayNumber, callback) {
-    $.get("http://" + host + "/status/" + [relayNumber], callback)
-}
--- server.js
+++ server.js
@@ -1,57 +1,77 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const port = 3000;
-const Gpio = require('onoff').Gpio;
-const relays = [new Gpio(17, 'high'), new Gpio(18, 'high'),new Gpio(27, 'high'),new Gpio(22, 'high')];
-const server = app.listen(port);
-const io = require('socket.io').listen(server);
+const passport = require('passport'),
+  BasicStrategy = require('passport-http').BasicStrategy,
+  key = require("./key.json"),
+  express = require('express'),
+  app = express(),
+  http = require('http'),
+  port = 3000,
+  Gpio = require('onoff').Gpio,
+  relays = [new Gpio(17, 'high'), new Gpio(18, 'high'), new Gpio(27, 'high'), new Gpio(22, 'high')],
+  server = app.listen(port),
+  io = require('socket.io').listen(server);
+
+passport.use(new BasicStrategy(
+  function(userid, password, done) {
+    User.findOne({ username: userid }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
 app.get('/status/:relayNumber', function (req, res) {
-  let relayNumber = parseInt(req.params.relayNumber); 
+  let relayNumber = parseInt(req.params.relayNumber);
   let status;
   console.log('Get status of relay #' + relayNumber);
   status = relays[relayNumber].readSync();
-  console.log((status === 0 ? 'ON':'OFF'));
+  console.log((status === 0 ? 'ON' : 'OFF'));
   res.send({
-    message:'Status of relay #'+ relayNumber + ' = ' + (status === 0 ? 'ON':'OFF'),
-    relayStatus: (status === 0 ? 'ON':'OFF')
+    message: 'Status of relay #' + relayNumber + ' = ' + (status === 0 ? 'ON' : 'OFF'),
+    relayStatus: (status === 0 ? 'ON' : 'OFF')
   });
 });
 
 app.post('/on/:relayNumber', function (req, res) {
-  let relayNumber = parseInt(req.params.relayNumber);  
+  let relayNumber = parseInt(req.params.relayNumber);
   console.log('Turn ON relay #' + relayNumber);
   relays[relayNumber].writeSync(0);
-  io.emit('message', 'Relay #' + (relayNumber+1) + ' was turned ON');
-  res.send('Relay #' + (relayNumber+1) + ' was turned ON');;
+  io.emit('message', 'Relay #' + (relayNumber + 1) + ' was turned ON');
+  io.emit('statuschange', {
+    relayNumber: relayNumber,
+    relayStatus: 0
+  });
+  res.send('Relay #' + (relayNumber + 1) + ' was turned ON');;
 });
 
 app.post('/off/:relayNumber', function (req, res) {
   let relayNumber = parseInt(req.params.relayNumber);
   console.log('Turn OFF relay #' + relayNumber);
   relays[relayNumber].writeSync(1);
-  io.emit('message', 'Relay #' + (relayNumber+1) + ' was turned OFF');
-  res.send('Relay #' + (relayNumber+1) + ' was turned OFF');
+  io.emit('message', 'Relay #' + (relayNumber + 1) + ' was turned OFF');
+  io.emit('statuschange', {
+    relayNumber: relayNumber,
+    relayStatus: 1
+  });
+  res.send('Relay #' + (relayNumber + 1) + ' was turned OFF');
 });
 
 app.use(express.static('public'));
 app.use('/libs', express.static('node_modules'));
 
-
-
-// Sockets
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', 'You are connected!');
-    socket.broadcast.emit('message', 'Another client has just connected!');
 
-    socket.on('message', function (message) {
-        console.log('A client is speaking to me! They\'re saying: ' + message);
-    }); 
+  for (index in relays) {
+    io.emit('statuschange', {
+      relayNumber: index,
+      relayStatus: relays[index].readSync()
+    });
+  }
+  socket.emit('message', 'You are connected!');
+  socket.broadcast.emit('message', 'Another client has just connected!');
+
+  socket.on('message', function (message) {
+    console.log('A client is speaking to me! They\'re saying: ' + message);
+  });
 });
